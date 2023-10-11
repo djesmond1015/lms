4eca19ed9abba133f793cce6d4d014a3a48c1d01
Desmond Lee <lee.desmond2016@gmail.com>
2023-10-11 16:28:46 +0800

    Add order model

---
+++ server/models/order.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Document, Model, Schema } from 'mongoose';
+
+export interface IOrder extends Document {
+  courseId: string;
+  userId: string;
+  payment_info: string;
+}
+
+const orderSchema = new Schema<IOrder>({
+  courseId: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  payment_info: Object,
+});
+
+const orderModel: Model<IOrder> = mongoose.model('Order', orderSchema);
+
+export default orderModel;
