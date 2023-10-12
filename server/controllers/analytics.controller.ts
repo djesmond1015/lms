0040900f6604cdced1f8de3799c64184325f7b43
Desmond Lee <lee.desmond2016@gmail.com>
2023-10-12 09:34:55 +0800

    Add order and notification analytics API

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -4,6 +4,8 @@ import ErrorHandler from '../utils/ErrorHandler';
 
 import { generateLast12MonthsData } from '../utils/analytics.generator';
 import userModel from '../models/user.model';
+import orderModel from '../models/order.model';
+import NotificationModel from '../models/notification.model';
 
 // Get users analytics -- admin
 export const getUsersAnalytics = CatchAsyncError(
@@ -14,6 +16,38 @@ export const getUsersAnalytics = CatchAsyncError(
       res.status(200).json({
         success: true,
         users,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// Get orders analytics -- admin
+export const getOrdersAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const orders = await generateLast12MonthsData(orderModel);
+
+      res.status(200).json({
+        success: true,
+        orders,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// Get notifications analytics -- admin
+export const getNotificationsAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const notifications = await generateLast12MonthsData(NotificationModel);
+
+      res.status(200).json({
+        success: true,
+        notifications,
       });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
--- server/routes/analytics.route.ts
+++ server/routes/analytics.route.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import { authorizeRoles, isAuthenticated } from '../middleware/auth';
 import { UserRoles } from '../models/user.model';
-import { getUsersAnalytics } from '../controllers/analytics.controller';
+import {
+  getNotificationsAnalytics,
+  getOrdersAnalytics,
+  getUsersAnalytics,
+} from '../controllers/analytics.controller';
 
 const analyticsRouter = express.Router();
 
@@ -10,6 +14,20 @@ analyticsRouter.get(
   isAuthenticated,
   authorizeRoles(UserRoles.ADMIN),
   getUsersAnalytics
+);
+
+analyticsRouter.get(
+  '/analytics/orders',
+  isAuthenticated,
+  authorizeRoles(UserRoles.ADMIN),
+  getOrdersAnalytics
+);
+
+analyticsRouter.get(
+  '/analytics/notifications',
+  isAuthenticated,
+  authorizeRoles(UserRoles.ADMIN),
+  getNotificationsAnalytics
 );
 
 export default analyticsRouter;
